fcbfd578eb3d3b2665ec276eee84da5b4136574e
Bhagya8805 <bhagyashri.shinde3399@gmail.com>
2023-10-09 17:41:01 +0530

    First Commit

---
+++ Selenium.Practice1/src/BrowserOpening/ChromeOpening.java
@@ -0,0 +1,13 @@
+package BrowserOpening;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class ChromeOpening {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+EdgeDriver cdriver=new EdgeDriver();
+	}
+
+}
